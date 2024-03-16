2ab3d84d3e1fe2d94f05fa1c1f205eca606800f6
Md Masud Rana <83323510+masudranam@users.noreply.github.com>
2024-03-16 13:57:01 +0600

    Add files via upload

---
+++ testing/gen.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define print(a) for(auto x:a)cout<<x<<' ';cout<<'\n';
+#define debug(x) cout<<#x<<" "<<x<<endl
+#define all(a) (a).begin(),(a).end()
+#define sz(a) (int)(a.size())
+#define int   long long int
+#define endl '\n'
+#define ar array
+ 
+const int M = 1e9 + 7;
+const int N = 2e5 + 10;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+inline int random(int l, int r) {
+	return uniform_int_distribution<int>(l, r)(rng);
+}
+
+
+void solve(){
+		int n = random(1,10);
+		
+		cout << n <<endl;
+		for(int i = 0; i < n; i++){
+			cout << random(1,10) <<' ';
+		}
+		cout << endl;
+
+		
+}
+
+signed main() {
+   ios_base::sync_with_stdio (0);
+   cin.tie (0);
+
+   int t = 1;  // cin >> t;
+   for (int tc = 1; tc <= t; tc++) {
+	  //cout<<"Case "<<tc<<": ";
+	  solve();
+   }
+   return 0;
+}
+
