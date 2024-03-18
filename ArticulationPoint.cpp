56963380bcaedb3ef7461f4583385a902473195e
Md Masud Rana <mdmasudrana1999bd@gmail.com>
2024-03-18 15:23:36 +0600

    Updated

--- ArticulationPoint.cpp
+++ ArticulationPoint.cpp
@@ -1,10 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define print(a) for(auto x:a)cout<<x<<' ';cout<<'\n';
-#define debug(x) cout<<#x<<" "<<x<<endl
-#define int   long long int
-
 const int M = 1e9 + 7;
 const int N = 2e5 + 10;
 
@@ -36,7 +32,7 @@ signed main() {
    ios_base::sync_with_stdio (0);
    cin.tie (0);
 
-   int t = 1;   cin >> t;
+   int t = 1;  // cin >> t;
    for (int tc = 1; tc <= t; tc++) {
       //cout<<"Case "<<tc<<": ";
       solve();
