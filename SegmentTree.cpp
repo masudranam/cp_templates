e01bc93e148eea58fa6db3aefd9631f8acebab88
mdmasudrana <mdmasudrana1999bd@gmail.com>
2024-09-03 10:14:39 +0600

    updated

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int M = 1e9 + 7;
+
 struct node {
     int mn,s,mx,lz;
     node(){
-        mn = mx = s = lz = 0;
+        mx = -M, mn = M, s = 0, lz = -1;
     }
 };
 
@@ -21,73 +23,78 @@ struct ST {
         return res;
     }
 
-    void upd(int l1,int x,int i,int l2,int r2) {
-        if(l2==r2) {
+    void upd(int L,int x,int i,int l,int r) {
+        if(l==r) {
             t[i].s = x;
             return;
         }
-        int m2=(l2+r2)/2;
-        if(l1<=m2) upd(l1, x, 2*i, l2, m2);
-        else upd(l1, x, 2*i+1, m2+1, r2);
+        int m = (l+r)/2;
+        if(L <= m) upd(L, x, 2*i, l, m);
+        else upd(L, x, 2*i+1, m+1, r);
         t[i] = Merge(t[2*i], t[2*i+1]);
     }
     void upd(int l, int val){
         upd(l,val,1,0,n-1);
     }
 
-    node qry(int l1,int r1,int i,int l2,int r2) {
-        if(l2 >= l1 && r2 <= r1) return t[i];
-        if(l2 > r1 || r2 < l1)return node();
-        int m2=(l2+r2)/2;
+    node qry(int L,int R,int i,int l,int r) {
+        if(l >= L && r <= R) return t[i];
+        if(l > R || r < L)return node();
+        int m=(l+r)/2;
 
         node left , right;
-        if(l1 <= m2)left = qry(l1, r1, 2*i, l2, m2);
-        if(m2 < r1)right = qry(l1, r1, 2*i+1, m2+1, r2);
+        if(L <= m)left = qry(L, R, 2*i, l, m);
+        if(m < R)right = qry(L, R, 2*i+1, m+1, r);
         return Merge(left, right);
     }
     node qry(int l, int r){
         return qry(l,r,1,0,n-1);
     }
 
-    void app(int i,int x,int l2,int r2) {
-        t[i].mn += x;
-        t[i].s += x*(r2-l2+1);
-        t[i].lz += x;
+     void prop(int i, int l, int r){
+        if(t[i].lz != -1){  
+            t[i].s = t[i].lz * (r - l + 1);
+            if(l != r){
+                t[2*i].lz = t[2*i + 1].lz = t[i].lz;
+            }
+            t[i].lz = -1;
+        }       
     }
 
-    void psh(int i,int l2,int m2,int r2) {
-        app(2*i, t[i].lz, l2, m2);
-        app(2*i+1, t[i].lz, m2+1, r2);
-        t[i].lz = 0;
-    }
-
-    void upd1(int l1, int r1, int x, int i, int l2, int r2) {
-        if(l1 <= l2 && r2 <= r1) {
-            app(i, x, l2, r2);
+    void upd1(int L, int R, int x, int i, int l, int r) {
+        prop(i,l,r);
+        if(L > r || R < l || l > r) return;
+        if(L <= l && r <= R) {
+            t[i].lz = x;
+            prop(i,l,r);
             return;
         }
-        int m2=(l2+r2)/2;
-        psh(i, l2, m2, r2);
-        if(l1 <= m2) upd1(l1, r1, x, 2*i, l2, m2);
-        if(m2 < r1) upd1(l1, r1, x, 2*i+1, m2+1, r2);
+        int m=(l+r)/2;
+        upd1(L, R, x, 2*i, l, m);
+        upd1(L, R, x, 2*i+1, m+1, r);
         t[i] = Merge(t[2*i] , t[2*i+1]);
     }
-    void upd1(int l, int r, int val){
-        upd1(l, r, val, 1, 0, n-1);
+
+    void upd1(int L, int R, int x){
+        return upd1(L,R,x,1,0,n-1);
     }
 
-    node qry1(int l1, int r1, int i, int l2, int r2) {
-        if(l1<=l2&&r2<=r1)return t[i];
-        int m2=(l2+r2)/2;
-        psh(i,l2,m2,r2);
+    node qry1(int L, int R, int i, int l, int r) {
+        prop(i,l,r);
+        if(L > r || R < l || l > r) return node();
+        if(L<=l && r<=R){
+            return t[i];
+        }
+        int m = (l+r)/2;
         node left , right;
-        if(l1 <= m2) left = qry1(l1, r1, 2*i, l2, m2);
-        if(m2 < r1) right = qry1(l1, r1, 2*i+1, m2+1, r2);
+         left = qry1(L, R, 2*i, l, m);
+         right = qry1(L, R, 2*i+1, m+1, r);
         return Merge(left, right);
     }
-    node qry1(int l, int r){
-        return qry1(l, r, 1, 0, n-1);
-    }
+
+    node qry1(int L, int R){
+        return qry1(L,R,1,0,n-1);
+    }    
 };
 
 void solve(){
