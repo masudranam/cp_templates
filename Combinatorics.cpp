c28fb33df9bc23c685519d1de6ec4d3839674234
mdmasudrana <mdmasudrana1999bd@gmail.com>
2024-09-03 10:11:05 +0600

    updated

--- Combinatorics.cpp
+++ Combinatorics.cpp
@@ -2,31 +2,53 @@
 using namespace std;
 
 const int M = 1e9 + 7;
+const int N = 2e5 + 10;
 
-struct Combi{
-    int n; vector<int> f, finv, inv;
-    int pw(int a, int b){
-        int res = 1;
-        while(b){
-            if(b&1)res = (res*a)%M;
-            a = (a*a) % M;
-            b >>= 1;
-        }
-        return res;
-    }
-
-   Combi(int _n): n(_n), f(_n), finv(_n), inv(_n){
-    inv[1] = f[0] = finv[0] =  1;
-    for(int i = 2; i < n; i++) inv[i] =  (inv[M % i] * (-M / i) % M + M) % M;
-    for(int i = 1; i < n; i++) f[i] = (f[i - 1] * i)%M;
-    for(int i = 1; i < n; i++) finv[i] = (finv[i-1]*inv[i])%M;
-   //finv[n-1] = pw(f[n-1],M-2);
-   //for(int i = n-2; i >= 0; i--)
-   // finv[i] = (finv[i + 1] * (i+1))%M;
-  }
-  inline int ncr(int n, int r) { return n<r?0:(f[n]*finv[r]%M*finv[n-r])%M;}
-  inline int npr(int n, int r) {return n<r?0:(f[n]*finv[n-r]%M);}
+template <const int32_t MOD>
+struct modint {
+  int32_t value;
+  modint() = default;
+  modint(int32_t value_) : value(value_) {}
+  inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+  inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+  inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+  inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+  inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+  inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+  inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+  modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
+  modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+  inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+  inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+  inline bool operator == (modint<MOD> other) const { return value == other.value; }
+  inline bool operator != (modint<MOD> other) const { return value != other.value; }
+  inline bool operator < (modint<MOD> other) const { return value < other.value; }
+  inline bool operator > (modint<MOD> other) const { return value > other.value; }
 };
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> istream & operator >> (istream & in, modint<MOD> &n) { return in >> n.value; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+ 
+using mint = modint<M>;
+ 
+struct Combi{
+  int n; vector<mint> facts, finvs, invs;
+  Combi(int _n): n(_n), facts(_n), finvs(_n), invs(_n){
+    facts[0] = finvs[0] = 1;
+    invs[1] = 1;
+      for (int i = 2; i < n; i++) invs[i] =  invs[M % i] * (-M / i);
+    for(int i = 1; i < n; i++){
+      facts[i] = facts[i - 1] * i;
+      finvs[i] = finvs[i - 1] * invs[i];
+    }
+  }
+  inline mint fact(int n) { return facts[n]; }
+  inline mint finv(int n) { return finvs[n]; }
+  inline mint inv(int n) { return invs[n]; }
+  inline mint ncr(int n, int k) { return n < k ? 0 : facts[n] * finvs[k] * finvs[n-k]; }
+};
+Combi C(N);
 
 void solve(){
    Combi C(10);
